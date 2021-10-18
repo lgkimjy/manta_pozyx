ece96615cdbf858a124c4bb9d22ec91cb5db76fa
lgkimjy <lgkimjy@naver.com>
2021-10-18 02:16:34 +0000

    data associate

--- manta_positioning/src/manta_positioning_association.cpp
+++ manta_positioning/src/manta_positioning_association.cpp
@@ -5,16 +5,23 @@
 
 #include "manta_positioning/manta_positioning_association.h"
 
+int num = 5;
+int flag = 0;
+
 void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
     pose = *msg;
     filtered_msg.data.push_back(pose);
+    flag++;
 }
 
 void controlFunction(const ros::TimerEvent&)
 {
-    pose_pub.publish(filtered_msg);
-    filtered_msg.data.clear();
+    if(flag < num){
+        pose_pub.publish(filtered_msg);
+        filtered_msg.data.clear();
+        flag = 0;
+    }
 }
 
 int main(int argc, char **argv){
@@ -23,18 +30,21 @@ int main(int argc, char **argv){
     ros::NodeHandle nh; 
     ros::Rate loop_rate(10);
 
-    ros::Subscriber sub1, sub2;
+    ros::Subscriber sub0, sub1, sub2, sub3, sub4;
     vector<ros::Subscriber> list;
 
+    list.push_back(sub0);
     list.push_back(sub1);
     list.push_back(sub2);
+    list.push_back(sub3);
+    list.push_back(sub4);
 
     for(int i=0; i<list.size(); i++){
         string data = "coord_" + to_string(i);
         list[i] = nh.subscribe(data, 10, poseCallback);
     }
 
-    ros::Timer timer_control = nh.createTimer(ros::Duration(0.025), controlFunction); // 25ms
+    ros::Timer timer_control = nh.createTimer(ros::Duration(0.01), controlFunction); // 10ms
 
     pose_pub = nh.advertise<manta_positioning::mqtt_msg>("/filtered/coord_list", 10);
 
