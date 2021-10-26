2d78c729f23f24f76142cdb6e32e3f471f4f4089
lgkimjy <lgkimjy@naver.com>
2021-10-26 23:55:32 +0900

    hpp modified

--- manta_positioning/include/manta_positioning/manta_positioning.h
+++ manta_positioning/include/manta_positioning/manta_positioning.h
@@ -21,18 +21,23 @@
 using namespace std;
 
 struct tag{
+    string id;
     float x;
     float y;
     float z;
+    float x_alpha;
+    float y_alpha;
+    float z_alpha;
+    float prev_raw_value;
 };
+
+vector<tag> tags;
 
 // manta_positioning::mqtt_msg filtered_msg;
 geometry_msgs::PoseStamped pose;
 ros::Publisher pose_pub;
 
-int tag_id;
-
-float raw_value, prev_raw_value, prev_avg_value;
+float prev_avg_value;
 float x_alpha, y_alpha, z_alpha;
 float xoffset, yoffset, zoffset;
 int flag=0;
@@ -40,7 +45,6 @@ int count=0;
 vector<float> data_buff;
 
 void readConfigData();
-void setOffset(tag data);
-float LowPassFilter(float raw_value, float alpha);
+float LowPassFilter(tag tag_data, int index);
 float MovingAvgeFilter();
 void poseCallback(const manta_positioning::mqtt_msg::ConstPtr& msg);
